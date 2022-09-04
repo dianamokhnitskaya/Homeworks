9590c4eb86998985fc6a5c66ea83f2dca953c37d
dianamokhnitskaya <123job345@gmail.com>
2022-09-04 23:25:35 +0300

    Task_5 Filter duplicate name from the list

---
+++ HW3_task5.py
@@ -0,0 +1,6 @@
+name_list = ["Salim Bird", "Salim Bird", "Salim Bird", "Salim Bird", "Salim Bird",
+             "Chanel Nielsen", "Chanel Nielsen", "Chanel Nielsen", "Chanel Nielsen", "Chanel Nielsen",
+             "Kaira Brook", "Kaira Brook", "Kaira Brook", "Kaira Brook", "Kaira Brook",
+             "Dustin Miles", "Dustin Miles", "Dustin Miles", "Dustin Miles", "Dustin Miles"]
+unique_names = list(dict.fromkeys(name_list))
+print(unique_names)
