b2fea7eca17b249a9cf2e2e17025fa09de97a4c3
dianamokhnitskaya <123job345@gmail.com>
2022-09-04 23:23:57 +0300

    Task_4 Vip boxes and common room

---
+++ HW3_task4.py
@@ -0,0 +1,8 @@
+vip_guests = ["Korben Brook", "Travis Velez", "Caio Schmidt", "Mateo Jeffery", "Aleah Boyce"]
+vip_box = frozenset(vip_guests)
+common_room = ["Lilian Davidson", "Malcolm Day", "Zain Gamble", "Mikaela Mansell"
+               "Ira Abbott", "Zofia Warner", "Kyle Chan"]
+free_space_in_common_room = [" ", " ", " ", " ", " ", " "]
+common_room.extend(free_space_in_common_room)
+print(common_room)
+
